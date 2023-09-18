e1b77a53aa0ecfd55e937dc19ee29e8b322b1df8
chandrashekarkandepu <chowschandra@gmail.com>
2023-09-17 22:31:01 -0400

    commit

--- src/main/java/com/example/Fullstack/controllers/EmployeeController.java
+++ src/main/java/com/example/Fullstack/controllers/EmployeeController.java
@@ -12,7 +12,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
