005351fa31f500aed2336a9c2da31775c9ed96f2
chandrashekarkandepu <34734568+chandrashekarkandepu@users.noreply.github.com>
2023-09-27 14:03:43 -0400

    Update FullstackApplicationTests.java

--- src/test/java/com/example/Fullstack/FullstackApplicationTests.java
+++ src/test/java/com/example/Fullstack/FullstackApplicationTests.java
@@ -20,42 +20,7 @@ class FullstackApplicationTests {
 	void contextLoads() {
 	}
 
-	@Test
-	public void imageForLoop(){
-
-		String folderPath = "/Users/chandrashekarkandepu/Downloads/FullstackApplication/Fullstack/src/test/resources/static"; // Replace with the actual folder path
-
-		File folder = new File(folderPath);
-
-		if (folder.exists() && folder.isDirectory()) {
-			File[] imageFiles = folder.listFiles(new FilenameFilter() {
-				@Override
-				public boolean accept(File dir, String name) {
-					String lowercaseName = name.toLowerCase();
-					return lowercaseName.endsWith(".jpg") || lowercaseName.endsWith(".png"); // Add more extensions if needed
-				}
-			});
-
-			if (imageFiles != null) {
-				for (File imageFile : imageFiles) {
-					// Process the image file (perform actions as needed)
-						System.out.println("Processing image file: " + imageFile.getName());
-					Assertions.assertEquals(1,1,"matched");
-
-				}
-			} else {
-				System.out.println("No image files found in the folder.");
-			}
-		} else {
-			System.out.println("The folder does not exist or is not a directory.");
-		}
-		System.out.println(employeeService.getTxt());
-
-	}
-	@Test
-	public void gettext(){
-		System.out.println(employeeService.getTxt());
-	}
+	
 
 
 }
